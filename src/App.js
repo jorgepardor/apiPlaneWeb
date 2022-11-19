8f332e0bed0835bd9d65c31f55b77570d069da61
Jorge Pardo <hi@jorgepardo.dev>
2022-11-19 21:04:36 +0100

    added SASS as css preprocessor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+// import './index.scss';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React <span className='test'>Hola</span>
         </a>
       </header>
     </div>
