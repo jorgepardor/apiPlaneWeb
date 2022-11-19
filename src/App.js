4d95884f6e948e1db4b51ea7a31044da2cca25aa
Jorge Pardo <hi@jorgepardo.dev>
2022-11-19 21:12:08 +0100

    added bootstrap 5

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link mt-4"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React <span className='test'>Hola</span>
+          Learn React <span className='test pl-2'>Hola</span>
         </a>
       </header>
     </div>
