715592b6b56c30d7d47b7fb74b1090dbd09df7e4
Jorge Pardo <hi@jorgepardo.dev>
2022-11-20 01:01:52 +0100

    enhanced flight component created

--- src/components/planeEnhanced.js
+++ src/components/planeEnhanced.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { logoSmall } from "../images/logos/owl_small.svg";
- 
 
 var bas_flight = {
   x407176: [
@@ -34,99 +32,151 @@ var enh_flight = {
     from: { from: "ABZ", name: "Aberdeen", time: "..." },
     to: { from: "TFS", name: "Tenerife", time: "..." },
   },
-  position: {lat:43.2041, lon:-8.0519, },
-  aircraft: { type: "Boeing 767-38A (ER)", registration: "G-TAWN", serial: 88333, country: "England", flag:"imagepath"},
-  recent: {"19/10/2022": ["BY1619", "TFS", "Tenerife", "EDI", "Edingburg"], "17/10/2022": ["BY1618", "EDI", "Edingburg", "TFS", "Tenerife"] },
-  data: { c_altitude: 37000, gps_altitude: 38788, v_speed: 0, track: 184, groundspeed: 922, mach: 1}
-
+  position: { lat: 43.2041, lon: -8.0519 },
+  aircraft: {
+    type: "Boeing 767-38A (ER)",
+    registration: "G-TAWN",
+    serial: 88333,
+    country: "England",
+    flag: "imagepath",
+  },
+  recent: {
+    "19/10/2022": ["BY1619", "TFS", "Tenerife", "EDI", "Edingburg"],
+    "17/10/2022": ["BY1618", "EDI", "Edingburg", "TFS", "Tenerife"],
+  },
+  data: {
+    c_altitude: 37000,
+    gps_altitude: 38788,
+    v_speed: 0,
+    track: 184,
+    groundspeed: 922,
+    mach: 1,
+  },
 };
 
 export const PlaneEnhanced = () => {
   return (
-    <div className="col-3 plane-info">
-      <div className="plane-info-box plane-info-header">
-        <div className="plane-info-header-ids">
-          <h5>BY2203 / TOM1DW</h5>
-          <p>TUI</p>
-          <p>Operated by TOU Airways</p>
+    <div className="col-12 col-md-4 px-2 px-md-0 plane-info">
+      <div className="plane-info-box">
+        <div className="plane-info-header">
+          <div className="plane-info-header-ids">
+            <h5>BY2203 / TOM1DW</h5>
+            <p>TUI</p>
+            <p>Operated by TOU Airways</p>
+          </div>
+          <div className="plane-info-header-logo">
+            <img
+              src="https://res.cloudinary.com/jorgepardor/image/upload/v1668892798/apiplaneweb/logo/Frame_jq2e5z.svg"
+              alt="radarowl logo"
+            />
+          </div>
         </div>
-        <div className="plane-info__box plane-info__header--logo">
-          <img src={logoSmall} alt="radarowl logo" />
+        <div className="plane-info-image">
+          <img
+            src="https://cdn.jetphotos.com/400/5/98629_1653709314.jpg"
+            alt="image of ..."
+          />
+        </div>
+        <div className="plane-info-route">
+          <div className="col-5 plane-info-route-item">
+            <h3 className="route-id">TFS</h3>
+            <p className="airport-name">Tenerife</p>
+            <p className="timezone">West (UTC+01:00)</p>
+          </div>
+          <div className="col-2 plane-info-route-icon">
+            <img src="https://res.cloudinary.com/jorgepardor/image/upload/v1668894413/apiplaneweb/logo/route_to_vqwant.svg" />
+          </div>
+          <div className="col-5 plane-info-route-item">
+            <h3 className="route-id">TFS</h3>
+            <p className="airport-name">Tenerife</p>
+            <p className="timezone">West (UTC+01:00)</p>
+          </div>
         </div>
       </div>
-      <div className="plane-info-box plane-info__image">
-        <img src="..." alt="image of ..." />
-      </div>
-      <div className="plane-info-box plane-info__route">
-        <div className="plane-info__route--box">
-          <h2>TFS</h2>
-          <p>Tenerife</p>
-          <p>West (UTC+01:00)</p>
-        </div>
-        <div className="plane-info__route--icon">
-          <img src="..." />
-        </div>
-        <div className="plane-info__route--box">
-          <h2>TFS</h2>
-          <p>Tenerife</p>
-          <p>West (UTC+01:00)</p>
-        </div>
-      </div>
-      <div className="plane-info-box plane-info__info">
-        <div className="plane-info__info--title">
+      <div className="plane-info-box">
+        <div className="plane-info-box-title">
+          <img
+            src="https://res.cloudinary.com/jorgepardor/image/upload/v1668897870/apiplaneweb/logo/aircraft-icon_ve82to.svg"
+            alt="aircraft info"
+          />
           <h5>More BY2203 information</h5>
         </div>
-        <div className="plane-info__info--box">
-          <div className="plane-info__info--aircraft">
-            <div className="plane-info__info--aircraft--type">
-              <p>AIRCRAFT TYPE (B763)</p>
-              <p>Boeing 767-38A (ER)</p>
+
+        <div className="plane-info-info">
+          <div className="plane-info-aircraft col-6">
+            <div className="plane-info-aircraft-item">
+              <p className="item">AIRCRAFT TYPE (B763)</p>
+              <p className="description">Boeing 767-38A (ER)</p>
             </div>
-            <div className="plane-info__info--aircraft--registration">
-              <p>REGISTRATION</p>
-              <p>G-OBYK</p>
-              <p>SERIAL NUMBER (MSN)</p>
+            <div className="plane-info-aircraft-item">
+              <p className="item">REGISTRATION</p>
+              <p className="description">G-OBYK</p>
+            </div>
+            <div className="plane-info-aircraft-item">
+              <p className="item">SERIAL NUMBER (MSN)</p>
+              <p className="description">43958407</p>
             </div>
           </div>
-          <div className="plane-info__info--airline">
-            <div className="plane-info__info--aircraft--info">
-              <img src="..." alt="airline ..." />
-              <div>
-                <p>COUNTRY OF REG.</p>
-                <img src="..." alt="airplane registered in ..." />
-              </div>
-              <div>
-                <p>AGE</p>
-                <p>XX YEARS</p>
-              </div>
+
+          <div className="plane-info-aircraft col-6">
+            <div className="airline-box">
+              <img
+                className="airline-logo"
+                src="https://res.cloudinary.com/jorgepardor/image/upload/v1668899829/apiplaneweb/temp/tui_r3ncxq.svg"
+                alt="airline ..."
+              />
+            </div>
+            <div className="plane-info-aircraft-flex">
+              <p className="item">COUNTRY OF REG.</p>
+              <img
+                src="https://res.cloudinary.com/jorgepardor/image/upload/v1668899588/apiplaneweb/temp/United_Kingdom_GB_mghuai.svg"
+                alt="airplane registered in ..."
+              />
+            </div>
+            <div className="plane-info-aircraft-item">
+              <p className="item">AGE</p>
+              <p className="description">XX Years</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="plane-info-box plane_info__recent">
-        <div className="plane-info__recent--title">
+
+      <div className="plane-info-box">
+        <div className="plane-info-box-title">
+          <img
+            src="https://res.cloudinary.com/jorgepardor/image/upload/v1668900532/apiplaneweb/logo/calendar_month_a3dymj.svg"
+            alt="recent flights"
+          />
           <h5>Recent G-OBYK flights</h5>
         </div>
-        <div className="plane-info__recent--box">
-          <div className="plane-info__recent--box--header">
-            <p>DATE</p>
-            <p>FLIGHT/ROUTE</p>
+        <div className="plane-info-recent">
+          <div className="plane-info-recent-header">
+            <p className="col-4 description">DATE</p>
+            <p className="col description">FLIGHT/ROUTE</p>
           </div>
-          <div className="plane-info__recent--box--flight">
-            <div className="plane-info__recent--box--flight--date">
-              <p>18/10/2022</p>
+          <div className="plane-info-recent-flight">
+            <div className="plane-info-recent-flight-date col-4">
+              <p className="date">18/10/2022</p>
             </div>
-            <div className="plane-info__recent--box--flight--route">
-              <p>BY2202(TOM56Y)</p>
-              <p>
+            <div className="plane-info-recent-flight-route col">
+              <p className="flight-id">
+                <strong>BY2202</strong> (TOM56Y)
+              </p>
+              <p className="airport">
                 <span>
-                  <img src="..." alt="route from ..." />
+                  <img
+                    src="https://res.cloudinary.com/jorgepardor/image/upload/v1668901140/apiplaneweb/logo/flght_from_fyf5go.svg"
+                    alt="route from ..."
+                  />
                 </span>
                 Manchester (MAN)
               </p>
-              <p>
+              <p className="airport">
                 <span>
-                  <img src="..." alt="route to ..." />
+                  <img
+                    src="https://res.cloudinary.com/jorgepardor/image/upload/v1668901141/apiplaneweb/logo/flight_to_lnqcjz.svg"
+                    alt="route to ..."
+                  />
                 </span>
                 Tenerife (TFS)
               </p>
@@ -134,56 +184,70 @@ export const PlaneEnhanced = () => {
           </div>
         </div>
       </div>
-      <div className="plane-info-box plane_info__extra">
-        <div className="plane-info__extra--title">
-          <h5>More BY2203 information</h5>
+
+      <div className="plane-info-box">
+        <div className="plane-info-box-title">
+          <img
+            src="https://res.cloudinary.com/jorgepardor/image/upload/v1668901795/apiplaneweb/logo/altitude_kw5iod.svg"
+            alt="recent flights"
+          />
+          <h5>Altitude</h5>
         </div>
-        <div className="plane-info__extra--box">
-          <div className="plane-info__extra--data">
-            <div className="plane-info__extra--altitude--calibrated">
-              <p>CALIBRATED ALTITUDE</p>
-              <p>36,000 ft</p>
+
+        <div className="plane-info-info">
+          <div className="plane-info-aircraft col-6">
+            <div className="plane-info-aircraft-item">
+              <p className="item">CALIBRATED ALTITUDE</p>
+              <p className="description">36,000 ft</p>
             </div>
-            <div className="plane-info__extra--altitude--gps">
-              <p>GPS ALTITUDE</p>
-              <p>N/A</p>
+            <div className="plane-info-aircraft-item">
+              <p className="item">GPS ALTITUDE</p>
+              <p className="description">36.000 ft</p>
             </div>
           </div>
-          <div className="plane-info__extra--data">
-            <div className="plane-info__extra--vspeed">
-              <p>Vertical speed </p>
-              <p>99999</p>
+
+          <div className="plane-info-aircraft col-6">
+            <div className="plane-info-aircraft-item">
+              <p className="item">Vertical speed</p>
+              <p className="description">00000</p>
             </div>
-            <div className="plane-info__extra--track">
-              <p>Track</p>
-              <p>24</p>
+            <div className="plane-info-aircraft-item">
+              <p className="item">Track</p>
+              <p className="description">184</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="plane-info-box plane_info__extra">
-        <div className="plane-info__extra--title">
-          <h5>More BY2203 information</h5>
+
+      <div className="plane-info-box">
+        <div className="plane-info-box-title">
+          <img
+            src="https://res.cloudinary.com/jorgepardor/image/upload/v1668901795/apiplaneweb/logo/altitude_kw5iod.svg"
+            alt="recent flights"
+          />
+          <h5>Speed</h5>
         </div>
-        <div className="plane-info__extra--box">
-          <div className="plane-info__extra--data">
-            <div className="plane-info__extra--altitude--calibrated">
-              <p>GROUND SPEED</p>
-              <p>464 kts</p>
+
+        <div className="plane-info-info">
+          <div className="plane-info-aircraft col-6">
+            <div className="plane-info-aircraft-item">
+              <p className="item">GROUND SPEED</p>
+              <p className="description">464 kts</p>
             </div>
-            <div className="plane-info__extra--altitude--gps">
-              <p>INDICATED AIRSPEED</p>
-              <p>N/A</p>
+            <div className="plane-info-aircraft-item">
+              <p className="item">INDICATED AIRSPEED</p>
+              <p className="description">N/A</p>
             </div>
           </div>
-          <div className="plane-info__extra--data">
-            <div className="plane-info__extra--vspeed">
-              <p>TRUE AIRSPEED</p>
-              <p>99999</p>
+
+          <div className="plane-info-aircraft col-6">
+            <div className="plane-info-aircraft-item">
+              <p className="item">TRUE AIRSPEED</p>
+              <p className="description">99999</p>
             </div>
-            <div className="plane-info__extra--track">
-              <p>MACH</p>
-              <p>24</p>
+            <div className="plane-info-aircraft-item">
+              <p className="item">MACH</p>
+              <p className="description">0.8</p>
             </div>
           </div>
         </div>
